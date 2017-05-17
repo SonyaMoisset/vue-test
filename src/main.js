93fc5221cb02d8410d6968ea2ca3640dbba7f85a
smoisset <smoisset@worldremit.com>
2017-05-17 19:02:22 +0200

    Update Vue Test SPA

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import Hello from './components/Hello'
+import About from './components/About'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Hello },
+  { path: '/about', component: About}
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+new Vue({
+  el: '#app',
+  template: `<App/>`,
+  components: {App},
+  router
+}).$mount('#app')
